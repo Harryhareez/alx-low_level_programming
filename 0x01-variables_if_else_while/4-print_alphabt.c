e32ef5c920bcefe41af53a052fb456fa813c2a63
harryhareez.harrisonhaniel <harryhareez.icent28@yahoo.com>
2023-02-17 10:40:19 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -20,24 +20,3 @@ int main(void)
         return (0);
 }
 
-
-
-5-print_numbers.c
-
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-
-int main(void)
-{
-        int a;
-
-        for (a = 0; a < 10; a++)
-                printf("%d", a);
-        putchar('\n');
-        return (0);
-}
-
