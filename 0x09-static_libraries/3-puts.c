91cf4123afedda06be73d6305bc754d8dd1bbacb
harryhareez.harrisonhaniel <harryhareez.icent28@yahoo.com>
2023-03-10 05:11:17 -0800

    libmy.a, main.h

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-void _puts(char *str);
-
 /**
  * main - Entry point
  *
@@ -11,13 +9,4 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
-
-/**
- * _puts - Prints a string to stdout
- * @str: The string to be printed
- */
-void _puts(char *str)
-{
-    /* Function body goes here */
 }
